4287ce36679750df9e23ce7882c4c3ce8e6c4ff2
MMoussa1212 <146038641+MMoussa1212@users.noreply.github.com>
2024-04-21 00:46:42 -0400

    Main code

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    boolean mainChecker = true;
-    Scanner mainScan = new Scanner(System.in);
-    while(mainChecker){
+        boolean mainChecker = true;
+        Scanner mainScan = new Scanner(System.in);
+        while(mainChecker){
             try{
                 throw new MyException();
             }
@@ -17,34 +17,34 @@ public class Main {
         }
     }
 }
-class MyException extends Throwable{
+class MyException extends Throwable {
 
 }
-abstract class Menus<T>{
-   abstract public T menuPromptAndSelect();
+abstract class Menus<T> {
+    abstract public T menuPromptAndSelect();
 
-  abstract public void menuSelectCheck();
+    abstract public void menuSelectCheck();
 }
 
-class MainMenu extends Menus<Integer>{
+class MainMenu extends Menus<Integer> {
     StudentManagement studentManagementMenu = new StudentManagement();
     CourseManagement courseManagementMenu = new CourseManagement();
     boolean whileToggle = true;
     @Override
-    public void menuSelectCheck(){
+    public void menuSelectCheck() {
         int userSelection  = -1;
         userSelection = menuPromptAndSelect();
         switch (userSelection){
             case 0->{ System.out.println("Goodbye!");
-            System.exit(0);}
-            case 1->studentManagementMenu.menuSelectCheck();
-            case 2->courseManagementMenu.menuSelectCheck();
+                System.exit(0);}
+            case 1->{studentManagementMenu.menuSelectCheck();}
+            case 2->{courseManagementMenu.menuSelectCheck();}
             default -> {System.out.println("fatal error! program will shutdown");System.exit(1);}
         }
 
     }
     @Override
-    public Integer menuPromptAndSelect(){
+    public Integer menuPromptAndSelect() {
         boolean toggle = true;
         int mainSelection = 0;
         while (toggle) {
@@ -57,7 +57,7 @@ class MainMenu extends Menus<Integer>{
                     throw new MyException();
                 }
             }
-            catch (MyException | Exception e){
+            catch (MyException | Exception e) {
                 System.out.println("invalid input!");
             }
             finally {
@@ -71,27 +71,27 @@ class MainMenu extends Menus<Integer>{
     }
 
 }
-class StudentManagement extends Menus<Character>{
+class StudentManagement extends Menus<Character> {
     //student management initial prompt code
     @Override
-    public Character menuPromptAndSelect(){
+    public Character menuPromptAndSelect() {
         boolean toggle = true;
         char studentSelection = ' ';
         while (toggle) {
             try {
                 Scanner studentScan = new Scanner(System.in);
-                System.out.println("a. add a student\nb. search for a student by id\nc. delete a student\nd. print the fee invoice of a student by id.\ne. Print all students");
+                System.out.println("a. add a student\nb. search for a student by id\nc. delete a student\nd. print the fee invoice of a student by id.\ne. Print all students"); // will change
                 studentSelection = studentScan.next().charAt(0);
                 studentScan.close();
-                if(studentSelection != ('a'|'b'|'c'|'d'|'e')){
+                if(studentSelection != ('a'|'b'|'c'|'d'|'e')){ // will change
                     throw new MyException();
                 }
             }
-            catch (MyException | Exception e){
+            catch (MyException | Exception e) {
                 System.out.println("invalid input!");
             }
             finally {
-                if(studentSelection ==('a'|'b'|'c'|'d'|'e') ){
+                if(studentSelection ==('a'|'b'|'c'|'d'|'e') ) { // will change
                     toggle = false;
                 }
             }
@@ -106,7 +106,7 @@ class StudentManagement extends Menus<Character>{
     }
 }
 
-class CourseManagement extends Menus<Integer>{
+class CourseManagement extends Menus<Integer> {
     @Override
     public Integer menuPromptAndSelect() {
         return 0;
@@ -117,31 +117,177 @@ class CourseManagement extends Menus<Integer>{
 
     }
 }
-abstract class Student{
-abstract public void printInvoice();
+
+// delete these comments later
+// All students: name and ID
+// Undergrad: undergrad courses taken, gpa, resident or not
+
+// MS: grad courses taken[],
+// PHD: labs supervised (discount), advisor, research subject
+// separate research fee of 700 for PHD students
+
+abstract class Student {
+
+    // Data
+    private String name;
+    private String id;
+
+    // Setters and Getters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    // Constructor
+    public Student(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return id.toUpperCase() + "-" + name.toUpperCase();
+    }
+
+    abstract public void printInvoice();
 }
 
 class UndergraduateStudent extends Student{
-    public void printInvoice(){
 
+    // Data
+    private int[] undergradCrnsTaken;
+    private double gpa;
+    private boolean resident;
+
+    // Setters and getters
+    public int[] getUndergradCrnsTaken() {
+        return undergradCrnsTaken;
+    }
+
+    public void setUndergradCrnsTaken(int[] undergradCrnsTaken) {
+        this.undergradCrnsTaken = undergradCrnsTaken;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public boolean isResident() {
+        return resident;
+    }
+
+    public void setResident(boolean resident) {
+        this.resident = resident;
+    }
+
+    // Constructor
+    public UndergraduateStudent(String name, String id, int[] undergradCrnsTaken, double gpa, boolean resident) {
+        super (name, id);
+        this.undergradCrnsTaken = undergradCrnsTaken.clone();
+        this.gpa = gpa;
+        this.resident= resident;
+    }
+
+    // Function to get cost for credit hours depending on if OOS resident or not
+    // ...
+
+    @Override
+    public void printInvoice() {
+        /// ...
     }
 }
 
-abstract class GraduateStudent extends Student{
+abstract class GraduateStudent extends Student {
 
-}
-
-class PHDStudent extends GraduateStudent{
-    public void printInvoice(){
-
+    public GraduateStudent (String name, String id) {
+        super(name, id);
     }
-}
-class  MSStudent extends GraduateStudent{
-    public void printInvoice(){
-
     }
-}
 
-class Courses{
+    class PHDStudent extends GraduateStudent {
 
-}
+        // Data
+        private int[] labsSupervised;
+        private String advisor;
+        private String researchSubject;
+
+        // Setters and Getters
+        public int[] getlabsSupervised() {
+            return labsSupervised;
+        }
+
+        public void setLabsSupervised(int[] labsSupervised) {
+            this.labsSupervised = labsSupervised;
+        }
+
+        public String getAdvisor() {
+            return advisor;
+        }
+
+        public void setAdvisor(String advisor) {
+            this.advisor = advisor;
+        }
+
+        public String getResearchSubject() {
+            return researchSubject;
+        }
+
+        public void setResearchSubject(String researchSubject) {
+            this.researchSubject = researchSubject;
+        }
+
+        // Constructor
+        public PHDStudent(String name, String id, int[] labsSupervised, String advisor, String researchSubject) {
+            super(name, id);
+            this.labsSupervised = labsSupervised.clone();
+            this.advisor = advisor;
+            this.researchSubject = researchSubject;
+        }
+
+        // Function to get total cost for a PHD student
+        // ...
+
+        @Override
+        public void printInvoice() {
+            // ...
+        }
+    }
+    class  MSStudent extends GraduateStudent {
+
+        // Data
+        private int[] gradCrnsTaken;
+
+        // Setter and getter
+        public int[] getGradCrnsTaken() {
+            return gradCrnsTaken;
+        }
+
+        public void setGradCrnsTaken(int[] gradCrnsTaken) {
+            this.gradCrnsTaken = gradCrnsTaken;
+        }
+
+        // Constructor
+        public MSStudent(String name, String id, int[] gradCrnsTaken) {
+            super(name, id);
+            this.gradCrnsTaken = gradCrnsTaken.clone();
+
+        }
+        public void printInvoice(){
+            // ...
+        }
+    }
